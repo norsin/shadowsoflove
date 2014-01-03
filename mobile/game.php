4008e58dc991348d8d2c2ab5564a664ce4aa762b
norsin <nt@bilandia.de>
2014-01-03 16:46:49 +0100

    #6690 Change Mobile Stuff

--- mobile/game.php
+++ mobile/game.php
@@ -4,17 +4,17 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width">
-        <title>Schräg verliebt</title>
-        <meta name="description" content="description text" />
-        <link rel="image_src" href="">
-        <meta property="og:image" content=""/>
-        <meta property="og:image:secure_url" content="">
+        <title>Shadows of Love</title>
+        <meta name="description" content="Shadows of Love - Jetzt Fan werden und ein Verwöhnpaket für sinnliche Stunden zu zweit gewinnen!" />
+        <link rel="image_src" href="img/share.jpg">
+        <meta property="og:image" content="http://f-bilandia.de/luebbe/shadowsoflove/img/share.jpg"/>
+        <meta property="og:image:secure_url" content="https://f-bilandia.de/luebbe/shadowsoflove/img/share.jpg">
         <meta property="og:image:type" content="image/jpg">
         <meta property="og:image:width" content="200">
         <meta property="og:image:height" content="200">
-        <meta property="og:title" content="og title"/>
-        <meta property="og:description" content="og description text" />
-        <meta property="og:url" content=""/>
+        <meta property="og:title" content="Shadows of Love"/>
+        <meta property="og:description" content="Lasst euch entführen – zu den geheimnisvollen Ufern der Leidenschaft, mit der neuen E-Book-Serie „Shadows of Love“ – Jetzt gewinnen!" />
+        <meta property="og:url" content="http://www.f-bilandia.de/luebbe/shadowsoflove/index.php"/>
         <link rel="stylesheet" href="css/frameset.css">
         </head>	
 	<body class="home">
@@ -24,12 +24,12 @@
                     <a href="start.php" class="losBtn button">Zu ersten Folge</a>
                 </div>
                 <div class="content">
-		    <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at nisi id augue suscipit sagittis tincidunt non tellus. Suspendisse ac arcu eu nibh placerat molestie eu nec nisl. Nunc vitae mauris turpis. Etiam ut auctor magna. Quisque feugiat turpis lorem, ut feugiat nunc bibendum vitae. Praesent consectetur interdum massa eu ultricies. Suspendisse potenti. In hac habitasse platea dictumst</p>
- <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at nisi id augue suscipit sagittis tincidunt non tellus. Suspendisse ac arcu eu nibh placerat molestie eu nec nisl. Nunc vitae mauris turpis. Etiam ut auctor magna. Quisque feugiat turpis lorem, ut feugiat nunc bibendum vitae. Praesent consectetur interdum massa eu ultricies. Suspendisse potenti. In hac habitasse platea dictumst</p>
- <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at nisi id augue suscipit sagittis tincidunt non tellus. Suspendisse ac arcu eu nibh placerat molestie eu nec nisl. Nunc vitae mauris turpis. Etiam ut auctor magna. Quisque feugiat turpis lorem, ut feugiat nunc bibendum vitae. Praesent consectetur interdum massa eu ultricies. Suspendisse potenti. In hac habitasse platea dictumst</p>
+		    <p class="text">In unserem Gewinnspiel verlosen wir ein Verwöhnpaket für sinnliche Stunden zu zweit inkl. der ersten Folge von „Shadows of Love“. Dafür möchten wir von euch wissen:</p>
+            <p class="text">Wie heißen die beiden Hauptpersonen in der ersten Folge von „Shadows of Love“?</p>
+            <p class="text">Schickt uns die richtige Lösung per E-Mail und dem Betreff „Shadows of Love“ bis zum 28.02.2014 an gewinnspiel@luebbe.de. Bitte gebt euren Namen und eure Postadresse an.</p>
                     <div class="btnRow">
-                        <a href="mailto:facebook@luebbe.de?subject=Shadows%20of%20Love" target="_blank" class="mitmachen button fullWidth">Mitmachen</a>
-			<a href="#" target="_blank" class="back button fullWidth">Zur&uuml;ck zu Facebook</a>
+                        <a href="mailto:gewinnspiel@luebbe.de?subject=Shadows%20of%20Love" class="mitmachen button fullWidth">Mitmachen</a>
+			            <a href="https://www.facebook.com/BasteiLuebbe" target="_blank" class="back button fullWidth">Zur&uuml;ck zu Facebook</a>
                     </div>
                 </div>
 
--- mobile/start.php
+++ mobile/start.php
@@ -4,17 +4,17 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width">
-        <title>Schräg verliebt</title>
-        <meta name="description" content="description text" />
-        <link rel="image_src" href="">
-        <meta property="og:image" content=""/>
-        <meta property="og:image:secure_url" content="">
+        <title>Shadows of Love</title>
+        <meta name="description" content="Shadows of Love - Jetzt Fan werden und ein Verwöhnpaket für sinnliche Stunden zu zweit gewinnen!" />
+        <link rel="image_src" href="img/share.jpg">
+        <meta property="og:image" content="http://f-bilandia.de/luebbe/shadowsoflove/img/share.jpg"/>
+        <meta property="og:image:secure_url" content="https://f-bilandia.de/luebbe/shadowsoflove/img/share.jpg">
         <meta property="og:image:type" content="image/jpg">
         <meta property="og:image:width" content="200">
         <meta property="og:image:height" content="200">
-        <meta property="og:title" content="og title"/>
-        <meta property="og:description" content="og description text" />
-        <meta property="og:url" content=""/>
+        <meta property="og:title" content="Shadows of Love"/>
+        <meta property="og:description" content="Lasst euch entführen – zu den geheimnisvollen Ufern der Leidenschaft, mit der neuen E-Book-Serie „Shadows of Love“ – Jetzt gewinnen!" />
+        <meta property="og:url" content="http://www.f-bilandia.de/luebbe/shadowsoflove/index.php"/>
         <link rel="stylesheet" href="css/frameset.css">
         </head>	
 	<body class="home">
@@ -25,12 +25,12 @@
                 </div>
                 <div class="content">
                     <img class="bookCover" src="images/bookcover.png">
-		    <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at nisi id augue suscipit sagittis tincidunt non tellus. Suspendisse ac arcu eu nibh placerat molestie eu nec nisl. Nunc vitae mauris turpis. Etiam ut auctor magna. Quisque feugiat turpis lorem, ut feugiat nunc bibendum vitae. Praesent consectetur interdum massa eu ultricies. Suspendisse potenti. In hac habitasse platea dictumst</p>
+		    <p class="text">Raumausstatterin Mia Ferris erhält vom reichsten Junggesellen Englands, dem Earl of Lindsay, das Angebot, einige Räume in seinem Schloss umzugestalten. Voller Ehrgeiz und auch aus Neugierde auf den Hausherrn nimmt sie den Auftrag an - nur der Geheimhaltungsvertrag, den sie vorab unterzeichnen muss, macht sie stutzig. Was hat Nathan Sterling zu verbergen?</p>
                     <div class="btnRow">
                         <a href="../Leseprobe.pdf" target="_blank" class="leseprobe button fullWidth">Leseprobe</a>
 			<a href="http://www.luebbe.de/Digital/Belletristik/Details/Id/978-3-8387-4790-
  24 3/?et_cid=71&et_lid=377913" target="_blank" class="mehr button fullWidth">Mehr zu &quot;Shadows of Love&quot;</a>
-			<a href="#" target="_blank" class="back button fullWidth">Zur&uuml;ck zu Facebook</a>
+			<a href="https://www.facebook.com/BasteiLuebbe" target="_blank" class="back button fullWidth">Zur&uuml;ck zu Facebook</a>
                     </div>
                 </div>
 
