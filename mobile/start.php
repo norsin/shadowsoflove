c2c57d618548ff448ddf17ec40d7e70107838f4f
Giovanni Canzio <gc@grin.com>
2014-01-03 14:09:14 +0100

    #6691: [Lübbe] Shadows of Love: mobil Programmierung (Start Dez.) - mobile homepage structure

--- mobile/start.php
+++ mobile/start.php
@@ -24,7 +24,7 @@
                     <a href="game.php" class="losBtn button">Zum Gewinnspiel</a>
                 </div>
                 <div class="content">
-                    <img class="bookCover" src="images/bookcover.png"><p class="Author">Cynthia Ceilán</p>
+                    <img class="bookCover" src="images/bookcover.png">
 		    <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at nisi id augue suscipit sagittis tincidunt non tellus. Suspendisse ac arcu eu nibh placerat molestie eu nec nisl. Nunc vitae mauris turpis. Etiam ut auctor magna. Quisque feugiat turpis lorem, ut feugiat nunc bibendum vitae. Praesent consectetur interdum massa eu ultricies. Suspendisse potenti. In hac habitasse platea dictumst</p>
                     <div class="btnRow">
                         <a href="#" target="_blank" class="leseprobe button fullWidth">Leseprobe</a>
