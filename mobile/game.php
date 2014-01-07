a258310b8458ca31fe5d933bca2455598e471873
norsin <nt@bilandia.de>
2014-01-07 16:44:03 +0100

    #6690 Add Teilnahmebedingungen

--- mobile/game.php
+++ mobile/game.php
@@ -16,6 +16,9 @@
         <meta property="og:description" content="Lasst euch entführen – zu den geheimnisvollen Ufern der Leidenschaft, mit der neuen E-Book-Serie „Shadows of Love“ – Jetzt gewinnen!" />
         <meta property="og:url" content="http://www.f-bilandia.de/luebbe/shadowsoflove/index.php"/>
         <link rel="stylesheet" href="css/frameset.css">
+        <script type="text/javascript" src="js/Libraries/jquery-1.8.2.min.js"></script>
+        <script type="text/javascript" src="js/main.js"></script>
+        <script type="text/javascript" src="js/Libraries/jquery-ui-1.10.3.min.js"></script>
         </head>	
 	<body class="home">
             <div class="wrapper">
@@ -32,7 +35,72 @@
 			            <a href="https://www.facebook.com/BasteiLuebbe" target="_blank" class="back button fullWidth">Zur&uuml;ck zu Facebook</a>
                     </div>
                 </div>
+<div class="box" id="teilnahmebox">
+                    <div id="close" class="closeBtn">x</div>
+                    <p>
+                            <u>
+                                Voraussetzung für die Teilnahme am Gewinnspiel
 
+                            </u>
+                        <br /><br />
+                            Voraussetzung für die Teilnahme am Gewinnspiel ist das Klicken auf den „Gefällt mir“-Button der Digital Darlings- oder Bastei Lübbe Facebook-Seite sowie eine E-Mail mit der richtigen Lösung an facebook@luebbe.de. Ausgeschlossen von der Teilnahme sind Mitarbeiter und Agenturen der Bastei Lübbe AG, sowie angeschlossener Unternehmen und deren Angehörige. 
+                        <br />
+                            Die Teilnahme ist nur natürlichen Personen gestattet. Eine Sammelteilnahme (insbesondere unter Einbeziehung hierauf ausgerichteter Unternehmen) ist ausdrücklich ausgeschlossen. Teilnehmer müssen persönlich mitspielen und dürfen sich keinerlei technischer Hilfsmittel bedienen. Besteht der Verdacht, dass doch Hilfsprogramme o. ä. benutzt worden sind, behält sich der Verlag vor, die Mitspieler von der Teilnahme auszuschließen.</p>
+                        <p>
+                            <u>
+                                Gewinne
+                            </u>
+                            <br /><br />
+                            Wir verlosen:
+                            <br /><br />
+                            1. Preis:
+                            Ein Verwöhnpaket für sinnliche Stunden zu zweit, bestehend aus<br />
+                            - Erste Folge von »Shadows of Love«<br />
+                            - Badepralinen<br />
+                            - Handschellen<br />
+                            - Body-Painting aus Schokolade<br />
+                            <br />
+                            2.- 5. Preis: 
+                            Die erste Folge von »Shadows of Love«
+                            <br /><br />
+                        </p>
+                        <p>
+                            <u>
+                                Ermittlung der Gewinner
+                            </u>
+                            <br /><br />
+                            Die Preise werden unter allen Einsendungen verlost. Das Gewinnspiel beginnt am XX.01.2014 und endet am 28.02.2014. Die Gewinner werden nach Ablauf des Gewinnspiels per E-Mail benachrichtigt. Auf die Benachrichtigung  weisen wir auf der Chronik der Facebook-Seite von Digital Darlings hin. Zum Abruf des Gewinns geben die Gewinner per E-Mail an facebook@luebbe.de ihren vollständigen Namen sowie ihre Adresse bekannt.
+                        </p>
+                        <p>
+                            <u>
+                                Datenschutz
+                            </u>
+                            <br /><br />
+                            <u>
+                                Nutzung von Daten durch Bastei Lübbe
+                            </u>
+                            <br /><br />
+                            Die uns übermittelten Daten werden nur für das Gewinnspiel und zur Benachrichtigung der Gewinner verwendet. Die namentliche Veröffentlichung endet nach vollständiger Beendigung und Abwicklung des Gewinnspiels. Die gespeicherten Daten werden anschließend gelöscht, soweit ein Teilnehmer nicht seine Einwilligung zur weiteren Nutzung der Daten erteilt hat.
+                        </p>
+                        <p>
+                            <u>
+                                Nutzung von Daten durch Facebook
+                            </u>
+                            <br /><br />
+                            Für unser Gewinnspiel nutzen wir die Plattform des sozialen Netzwerks facebook.com, das von der Facebook Ireland Ltd., Hanover Reach 5-7, Hanover Quay, Dublin 2 Irland, betrieben wird („Facebook“). Durch Ihre Teilnahme an diesem Gewinnspiel ist es nicht auszuschließen, dass Facebook Kenntnis über Ihre IP-Adresse erlangt. Alle weiteren Daten, die im Rahmen dieses Gewinnspiels erfasst werden, werden nicht an Facebook oder andere Dritte weitergegeben. Zweck und Umfang der Datenerhebung und die weitere Verarbeitung und Nutzung der Daten durch Facebook sowie ihre diesbezüglichen Rechte und Einstellungsmöglichkeiten zum Schutz Ihrer Privatsphäre entnehmen Sie bitte den Datenschutzhinweisen von Facebook: http://www.facebook.com/policy.php
+                        </p>
+                        <p>
+                            <u>
+                                Freistellung von Facebook
+                            </u>
+                            <br /><br />
+                            Veranstalter des Gewinnspiels ist allein die Bastei Lübbe AG. Das Gewinnspiel steht in keiner Verbindung zu Facebook und wird in keiner Weise von Facebook gesponsert, unterstützt oder organisiert. Das Gewinnspiel wird durch die bilandia GmbH ausgeführt.
+                        </p>
+                        <p><div class="closeBtn" id="close2">x</div></p>
+                </div>
+                
+            </div>
+            <div id="teilnahme">Teilnahmebedingungen</div>
               
 	</body>
 </html>
